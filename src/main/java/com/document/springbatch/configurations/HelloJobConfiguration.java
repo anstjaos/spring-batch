81bc431f705643c59b1e0258dc607510bbf10974
anstjaos <moonseo.kim@nhn.com>
2023-02-11 18:35:24 +0900

    Spring Batch 5에 맞게 버전 수정

--- src/main/java/com/document/springbatch/configurations/HelloJobConfiguration.java
+++ src/main/java/com/document/springbatch/configurations/HelloJobConfiguration.java
@@ -1,51 +1,48 @@
 package com.document.springbatch.configurations;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@RequiredArgsConstructor
 @Configuration
 public class HelloJobConfiguration {
-    private final JobBuilder jobBuilder;
-    private final StepBuilder stepBuilder;
-
     @Bean
-    public Job helloJob() {
-        return jobBuilder
-                .start(helloStep1())
-                .next(helloStep2())
+    public Job helloJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep1(jobRepository, transactionManager))
+                .next(helloStep2(jobRepository, transactionManager))
                 .build();
     }
 
     @Bean
-    public Step helloStep1() {
-        return stepBuilder
+    public Step helloStep1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("helloStep1", jobRepository)
                 .tasklet((stepContribution, chunkContext) -> {
                     System.out.println(" =======================");
                     System.out.println(" >> Hello Spring Batch!!");
                     System.out.println(" =======================");
 
                     return RepeatStatus.FINISHED;
-                })
+                }, transactionManager)
                 .build();
     }
 
     @Bean
-    public Step helloStep2() {
-        return stepBuilder
+    public Step helloStep2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("helloStep2", jobRepository)
                 .tasklet((stepContribution, chunkContext) -> {
                     System.out.println(" =======================");
                     System.out.println(" >> step2 was executed.");
                     System.out.println(" =======================");
 
                     return RepeatStatus.FINISHED;
-                })
+                }, transactionManager)
                 .build();
     }
 }
